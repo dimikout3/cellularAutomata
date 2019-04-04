02502848432e2d20e3db86c491bb3be1f9b960d9
dkoutras <dkoutras@certh.gr>
2019-04-04 12:19:25 +0300

    Added multiple enviroments

--- cellurarAutomata/task2/CellularAutomata.py
+++ cellurarAutomata/task2/CellularAutomata.py
@@ -9,6 +9,8 @@ class CellularAutomata:
         self.rows = rows
         self.columns = columns
         self.env = np.ones([rows,columns])
+        self.data = []
+        self.title = "Original Cycle"
 
     def dist(self, x1, y1, x2, y2):
         return math.sqrt( ((x1-x2)**2) + ((y1-y2)**2) )
@@ -25,12 +27,14 @@ class CellularAutomata:
                     self.env[x][y] = 0
 
     def printCA(self):
-        plt.imshow(self.env)
+        plt.imshow(self.env, cmap='gray')
         plt.colorbar()
         plt.show()
         plt.close()
 
     def applyNoiseBoundary(self, noiseLevel = 1):
+
+        self.title = "Boundary Noise - Noise Level: "+str(noiseLevel)
 
         (rows, columns) = np.where(self.env == 0)
 
@@ -53,8 +57,9 @@ class CellularAutomata:
             # print('boundaries are :',boundaries[i][0],boundaries[i][1])
             self.env[ boundaries[i][0] ][ boundaries[i][1]] = 1
 
-
     def applyNoiseRandom(self, noiseLevel = 1):
+
+        self.title = "Inner Noise - Noise Level: "+str(noiseLevel)
 
         (rows, columns) = np.where(self.env == 0)
         # pdb.set_trace()
@@ -65,7 +70,29 @@ class CellularAutomata:
                 self.env[ rows[randIndx] ][ columns[randIndx] ] = 1
                 noiseLevel -= 1
 
-    def step(self):
+    def plotData(self):
+        plt.plot(self.data)
+        plt.show()
+        plt.close()
+
+    def getData(self):
+        return self.data
+
+    def getTitle(self):
+        return self.title
+
+    def saveStep(self, timeStep = None):
+        plt.imshow(self.env, cmap='gray')
+        plt.colorbar()
+        plt.title(self.title+'\nTime Step: '+str(timeStep))
+        plt.savefig('results/'+self.title+' Time_Step_'+str(timeStep))
+        plt.close()
+
+    def step(self, timeStep = None):
+
+        self.saveStep(timeStep = timeStep)
+
+        self.data.append(np.count_nonzero(self.env))
 
         (rows, columns) = np.where(self.env == 0)
 
--- cellurarAutomata/task2/runner.py
+++ cellurarAutomata/task2/runner.py
@@ -1,6 +1,7 @@
 import numpy as np
 import optparse
 from CellularAutomata import CellularAutomata
+import matplotlib.pyplot as plt
 
 def get_options():
 
@@ -11,6 +12,8 @@ def get_options():
                          help="The X center of the cycle", type = "int")
     optParser.add_option("-y", dest="Y_center", help="The Y center of the cycle", default = 20, type = "int")
     optParser.add_option("-s", dest="size", help="The size of the enviroment", default = 40, type = "int")
+    optParser.add_option("-t", dest="time_steps", help="The total number of time steps", default = 6, type = "int")
+    optParser.add_option("-n", dest="noise_level", help="The number of cells with noise", default = 1, type = "int")
 
 
     options, args = optParser.parse_args()
@@ -21,14 +24,29 @@ def run2(options):
     rows = options.size
     columns = options.size
 
-    env = CellularAutomata(rows, columns)
+    enviroments = [CellularAutomata(rows, columns) for i in range(5)]
 
-    env.drawCycle(options.radius, options.X_center, options.Y_center)
-    env.applyNoiseRandom()
-    # env.printCA()
-    for i in range(6):
-        env.printCA()
-        env.step()
+    for i in enviroments: i.drawCycle(options.radius, options.X_center, options.Y_center)
+
+    enviroments[1].applyNoiseBoundary()
+    enviroments[2].applyNoiseBoundary(noiseLevel = options.noise_level)
+    enviroments[3].applyNoiseRandom()
+    enviroments[4].applyNoiseRandom(noiseLevel = options.noise_level)
+
+    for i in range(options.time_steps):
+        print("Time Step: ",i)
+        # env.printCA()
+        for env in enviroments: env.step(timeStep=i)
+
+    data = [env.getData() for env in enviroments]
+    titles = [env.getTitle() for env in enviroments]
+
+    for index, d in enumerate(data):
+        plt.plot(d, label=titles[index])
+    plt.legend()
+    plt.xlabel('Time Step')
+    plt.ylabel('Non Zero Elements [Activated Cells]')
+    plt.show()
 
 # this is the main entry point of this script
 if __name__ == "__main__":
